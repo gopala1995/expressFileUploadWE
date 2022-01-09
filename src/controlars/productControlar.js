9ced6064d3f28f2db5b6f282b28abcd1ee7041c8
Gopala <guptaraja727@gmail.com>
2022-01-10 02:12:48 +0530

    cc

--- src/controlars/productControlar.js
+++ src/controlars/productControlar.js
@@ -1,8 +1,7 @@
 const express = require("express")
-const path = require("path")
+const multer = require("multer")
 
-
-
+const upload = require("../middlewere/upload")
 const router = express.Router()
 
 const Product = require("../models/product.model")
@@ -13,8 +12,25 @@ router.get("",async(req,res)=>{
     return res.send(products)
 })
 
-router.post("",async(req,res)=>{
-    const product = await Product.create(req.body)
+router.post("/single",upload.single("image_urls"),async(req,res)=>{
+  
+    const product = await Product.create({
+      name:req.body.name,
+      price:req.body.price,
+      image_url:req.body.path
+      
+    })
     return res.send(product)
 })
+
+router.post("/multiple",upload.any("image_urls"),async(req,res)=>{
+  const filePath = req.files.map((file)=>file.path);
+  const product = await Product.create({
+    name:req.body.name,
+    price:req.body.price,
+    image_url:filePath
+  })
+  return res.send(product)
+})
+
 module.exports = router
--- src/middlewere/upload.js
+++ src/middlewere/upload.js
@@ -1,19 +1,36 @@
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '../uploadsFile'))
-    },
-    filename: function (req, file, cb) {
-      const uniqueprefix = Date.now();
-      cb(null, uniqueprefix + '-' + file.originalname )
-    }
-  })
-  
-  
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploadsFile"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  // The function should call `cb` with a boolean
+  // to indicate if the file should be accepted
+  
+  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    // To accept the file pass `true`, like so:
+    cb(null, true);
+  } else {
+    // To reject this file pass `false`, like so:
+    cb(null, false);
+  }
 
-const upload = multer({storage:storage,fileFilter:fileFilter,limits:{
-  fileSize: 1024 * 1024 * 5
-} })
+ 
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
+
+module.exports = upload
